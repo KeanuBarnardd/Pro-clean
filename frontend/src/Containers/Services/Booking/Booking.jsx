8600f6d0e473463347b16b04c64b3146c1bcecfe
Keanu Barnard <keanu.barnardd@gmail.com>
2022-10-19 22:54:55 +1100

    Date component start

--- frontend/src/Containers/Services/Booking/Booking.jsx
+++ frontend/src/Containers/Services/Booking/Booking.jsx
@@ -15,12 +15,12 @@ const Booking = () => {
 
   const [valueArrays, setValueArrays] = useState({
     stories: [1, 2, 3, 4],
-    rooms: [ 1, 2, 3, 4, 5, 6, 7, 8],
+    rooms: [1, 2, 3, 4, 5, 6, 7, 8],
     baths: [1, 2, 3, 4, 5, 6, 7, 8],
     // Sub Categories
     blinds: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
     walls: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    carpets: [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    carpets: [0, 1, 2, 3, 4, 5, 6, 7, 8],
   });
 
   const [propertyPriceData, setPropertyPriceData] = useState({
@@ -88,7 +88,11 @@ const Booking = () => {
           />
         </div>
       </div>
-    
+      <div className="app__container app__flex">
+        <div className="app__container-width">
+          <DateForm/>
+        </div>
+      </div>
     </>
   );
 };
--- frontend/src/Containers/Services/Booking/DateForm/DateForm.jsx
+++ frontend/src/Containers/Services/Booking/DateForm/DateForm.jsx
@@ -3,7 +3,14 @@ import React from "react";
 import "./DateForm.scss";
 
 const DateForm = () => {
-  return <div>DateForm</div>;
+  return <div className="date__form-container">
+    <p className="content__title"><span>STEP 2</span></p>
+    <h1 className="head-text">Date & Time</h1>
+
+
+  <input type="date" name="" id="" />
+
+  </div>;
 };
 
 export default DateForm;
